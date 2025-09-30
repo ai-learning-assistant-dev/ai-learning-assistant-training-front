701828d13bf2a2e3a518a2cbe126d131247d17ac
张卓诚 <shiftonetothree@126.com>
2025-10-01 01:16:18 +0800

    feat(*): add base component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import { Button } from "@/components/ui/button"
+import AiConversation from "@/components/ai-conversation"
  
 function App() {
   return (
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
+    <div className="flex w-full h-full flex-row gap-6">
+      <Tabs defaultValue="account" className="w-[400px] grow">
+        <TabsList>
+          <TabsTrigger value="account">学习页</TabsTrigger>
+          <TabsTrigger value="password">用户信息</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">学习中</TabsContent>
+        <TabsContent value="password">用户信息<Button>Save</Button></TabsContent>
+      </Tabs>
+      <div className="w-[400px] h-full shrink-0">
+        <AiConversation />
+      </div>
     </div>
   )
 }
