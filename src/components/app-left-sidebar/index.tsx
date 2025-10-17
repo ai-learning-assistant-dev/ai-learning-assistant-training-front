1f09fa59c7bdda040d4c1a0e631e5208d3c1dca1
张卓诚 <shiftonetothree@126.com>
2025-10-17 20:28:54 +0800

    feat(app-left-sidebar): add user info card

--- src/components/app-left-sidebar/index.tsx
+++ src/components/app-left-sidebar/index.tsx
@@ -1,49 +1,56 @@
-import { Calendar, Home, Inbox, Book, Search, Settings } from "lucide-react"
+import { Calendar, Home, Inbox, Book, Search, Settings, CircleQuestionMark, VideoIcon, Gauge, User } from "lucide-react"
 
 import {
   Sidebar,
   SidebarContent,
+  SidebarFooter,
   SidebarGroup,
   SidebarGroupContent,
   SidebarGroupLabel,
+  SidebarHeader,
   SidebarMenu,
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarProvider,
   SidebarTrigger,
 } from "@/components/ui/sidebar"
-
+import { Progress } from "@/components/ui/progress"
+import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 // Menu items.
-const items = [
+const topItems = [
   {
     title: "课程广场",
     url: "/app/courseList",
     icon: Book,
   },
   {
-    title: "Home",
+    title: "学习情况总揽",
     url: "#",
-    icon: Home,
+    icon: Gauge,
   },
   {
-    title: "Inbox",
-    url: "#",
-    icon: Inbox,
-  },
-  {
-    title: "Calendar",
+    title: "我的学习计划",
     url: "#",
     icon: Calendar,
   },
   {
-    title: "Search",
+    title: "课堂空间",
     url: "#",
-    icon: Search,
+    icon: VideoIcon,
   },
+]
+
+// Menu items.
+const bottomItems = [
   {
     title: "Settings",
     url: "#",
     icon: Settings,
+  },
+  {
+    title: "获得帮助",
+    url: "#",
+    icon: CircleQuestionMark,
   },
 ]
 
@@ -51,12 +58,26 @@ export function AppLeftSidebar({children}: {children?: React.ReactNode}) {
   return (
     <SidebarProvider>
       <Sidebar>
+        <SidebarHeader>
+          <Card>
+            <CardHeader>
+              <CardTitle><User size={24} style={{display: 'inline'}} />用户名</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <div className="flex justify-between items-center w-full">
+                <div>LV5</div>
+                <div>青铜电工</div>
+              </div>
+              <Progress value={30}></Progress>
+            </CardContent>
+          </Card>
+        </SidebarHeader>
+        
         <SidebarContent>
           <SidebarGroup>
-            <SidebarGroupLabel>Application</SidebarGroupLabel>
             <SidebarGroupContent>
               <SidebarMenu>
-                {items.map((item) => (
+                {topItems.map((item) => (
                   <SidebarMenuItem key={item.title}>
                     <SidebarMenuButton asChild>
                       <a href={item.url}>
@@ -70,6 +91,20 @@ export function AppLeftSidebar({children}: {children?: React.ReactNode}) {
             </SidebarGroupContent>
           </SidebarGroup>
         </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            {bottomItems.map((item) => (
+              <SidebarMenuItem key={item.title}>
+                <SidebarMenuButton asChild>
+                  <a href={item.url}>
+                    <item.icon />
+                    <span>{item.title}</span>
+                  </a>
+                </SidebarMenuButton>
+              </SidebarMenuItem>
+            ))}
+          </SidebarMenu>
+        </SidebarFooter>
       </Sidebar>
       <main>
         <SidebarTrigger />
