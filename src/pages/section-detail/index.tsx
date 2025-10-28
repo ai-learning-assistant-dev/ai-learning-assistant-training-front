efb92d7b7942a4d71b87524aea8eda7f40b8f7cf
张卓诚 <shiftonetothree@126.com>
2025-10-29 05:29:21 +0800

    feat(section-detail): add tabs

--- src/pages/section-detail/index.tsx
+++ src/pages/section-detail/index.tsx
@@ -9,15 +9,16 @@ import { Examination } from "@/components/examination";
 import type { Stage } from "@/components/section-stage";
 import { useState } from "react";
 import { getLoginUser } from "@/containers/auth-middleware";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
 
 export function SectionDetail() {
   let params = useParams();
   const [stage, setStage] = useState<Stage>('video');
   const [trigger, setTrigger] = useState(1);
   const { loading, error, data } = useAutoCache(sectionsServer.getById.bind(sectionsServer), [{ section_id: params.sectionId }]);
-  const {data: exerciseResult } = useAutoCache(
-    exerciseResultServer.getExerciseResults, 
-    [{user_id: getLoginUser()?.user_id,section_id: params.sectionId}], undefined, trigger
+  const { data: exerciseResult } = useAutoCache(
+    exerciseResultServer.getExerciseResults,
+    [{ user_id: getLoginUser()?.user_id, section_id: params.sectionId }], undefined, trigger
   );
   if (loading) {
     return <div>loading...</div>
@@ -26,19 +27,19 @@ export function SectionDetail() {
     return <div>{error.message}</div>
   }
 
-  const onPass = async (data: any)=>{
+  const onPass = async (data: any) => {
     setStage('compare')
   }
 
-  const onFail = async (data: any)=>{
+  const onFail = async (data: any) => {
     setStage('video')
   }
 
-  const changeStage = async (stage: Stage)=>{
-    if(stage === 'video' || stage === 'examination' ){
+  const changeStage = async (stage: Stage) => {
+    if (stage === 'video' || stage === 'examination') {
       setStage(stage)
-    }else{
-      if(exerciseResult?.data.pass){
+    } else {
+      if (exerciseResult?.data.pass) {
         setStage(stage);
       }
     }
@@ -49,10 +50,23 @@ export function SectionDetail() {
     return (
       <div className="flex flex-col gap-4 px-6">
         <SectionHeader />
-        <SectionStage stage={stage} onClick={changeStage}/>
+        <SectionStage stage={stage} onClick={changeStage} />
         {stage !== 'examination' && <VideoPlayer url={section.video_url} />}
-        {stage !== 'examination' && <Response className="text-base leading-relaxed">{section.knowledge_content}</Response>}
-        {stage !== 'video' && <Examination onPass={onPass} onFail={onFail} />}
+        {stage !== 'examination' && (<Tabs defaultValue="doc">
+          <TabsList>
+            <TabsTrigger value="doc">知识点文案</TabsTrigger>
+            {stage === 'compare' && <TabsTrigger value="examination">随堂测验</TabsTrigger>}
+          </TabsList>
+          <TabsContent value="doc">
+            <Response className="text-base leading-relaxed">{section.knowledge_content}</Response>
+          </TabsContent>
+          {stage === 'compare' && (
+            <TabsContent value="examination">
+              <Examination onPass={onPass} onFail={onFail} />
+            </TabsContent>
+          )}
+        </Tabs>)}
+        {stage === 'examination' && <Examination onPass={onPass} onFail={onFail} />}
       </div>
     )
   }
