d2a9fecd27d63d486e59109971202716abc565af
张卓诚 <shiftonetothree@126.com>
2025-11-09 17:41:45 +0800

    feat(course-list): add style for corse-list

--- src/pages/course-list/index.tsx
+++ src/pages/course-list/index.tsx
@@ -4,6 +4,9 @@ import { Button } from "@/components/ui/button";
 import { NavLink } from "react-router";
 import { Item, ItemActions, ItemContent, ItemDescription, ItemFooter, ItemHeader, ItemMedia, ItemTitle } from "@/components/ui/item";
 import { Book } from "lucide-react";
+import { Card, CardHeader, CardTitle, CardDescription, CardAction, CardContent, CardFooter } from "@/components/ui/card";
+import "./index.css"
+import { Image } from "@/components/ui/shadcn-io/ai/image";
 
 export function CourseList() {
   const { loading, error, data } = useAutoCache(courseServer.search.bind(courseServer), [{ limit: 10, page: 1 }]);
@@ -16,18 +19,18 @@ export function CourseList() {
   if (loading === false && error == null) {
     const courseList = data.data;
     return (
-      <div className="flex">
+      <div className="flex gap-4 p-4">
         {courseList.map((course) => (
-          <Item key={course.course_id} variant="outline">
-            <ItemMedia><Book /></ItemMedia>
-            <ItemContent>
-              <ItemTitle>{course.name}</ItemTitle>
-              <ItemDescription>{course.description}</ItemDescription>
-            </ItemContent>
-            <ItemActions>
-              <NavLink to={`/app/courseList/courseDetail/${course.course_id}`}><Button>学习</Button></NavLink>
-            </ItemActions>
-          </Item>
+          <NavLink key={course.course_id} to={`/app/courseList/courseDetail/${course.course_id}`}>
+            <Card className="w-[320px] h-[159px] course-card">
+              <CardHeader>
+                <CardTitle><Book className="inline"/>{course.name}<span className="time"></span></CardTitle>
+              </CardHeader>
+              <CardContent className="content">
+                {course.description}
+              </CardContent>
+            </Card>
+          </NavLink>
           ))}
         
 
