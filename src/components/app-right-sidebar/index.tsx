3874c0d2471ef6c06d7503bf8261c87d61bd1ac2
张卓诚 <shiftonetothree@126.com>
2025-11-20 00:24:25 +0800

    style(app-right-sidbar): fix style

--- src/components/app-right-sidebar/index.tsx
+++ src/components/app-right-sidebar/index.tsx
@@ -12,7 +12,7 @@ import "./index.css";
 
 export function AppRightSidebar() {
   return (
-    <div className="flex w-200 h-100% py-2 pr-2 bg-gray-50 h-svh">
+    <div className="flex grow-0 shrink-0 w-[515px] h-100% py-2 pr-2 bg-gray-50 h-svh">
       <AiConversation/>
     </div>
   )
