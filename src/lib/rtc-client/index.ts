336e91fc1503ce8b773b565eb4346fc52ea02d29
生于斯 <38646996+shengyusi-SYS@users.noreply.github.com>
2025-12-09 21:41:24 +0800

    feat(ai-chat):add mic selector and test

--- src/lib/rtc-client/index.ts
+++ src/lib/rtc-client/index.ts
@@ -3,17 +3,9 @@
  * 基于 Emittery 的 FastRTC WebRTC 通信库
  */
 
-export { FastRTCClient } from "./rtc-client";
-export type {
-  Message,
-  MessageType,
-  FastRTCClientConfig,
-  FastRTCClientEvents,
-  WebRTCError,
-  VisualizerConfig,
-  Subtitle,
-} from "./types";
+export { FastRTCClient } from './rtc-client';
+export type { Message, MessageType, FastRTCClientConfig, FastRTCClientEvents, WebRTCError, VisualizerConfig, Subtitle, MicrophoneDevice, MicrophoneTestStatus } from './types';
 
 // 音频波形可视化模块
-export { InputVisualizer } from "./input-visualizer";
-export { OutputVisualizer } from "./output-visualizer";
+export { InputVisualizer } from './input-visualizer';
+export { OutputVisualizer } from './output-visualizer';
